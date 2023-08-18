e415c2f8896824b1c2747314de5d50aa2f5b1455
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-08-18 17:20:06 +0530

    Implements live app with caching.
    App updates every 90 seconds:
    	trigger for fetch every 30s
    	update cache memory if older than 60s

--- src/App.jsx
+++ src/App.jsx
@@ -1,196 +1,110 @@
+
 import { useEffect, useState } from 'react'
 import './App.css'
 
+const cacheTimeKey = 'cache_time';
+const cacheCoins = 'cache_coins'
 
-const CACHE_KEY = 'cachedCoins';
-const CACHE_TIME = 'cachedTime';
+// async function myFetch() {
+//   console.log("fetch queried")
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve( [
+//         {
+//           id: 'bitcoin',
+//           current_price: 26431, 
+//         },
+//         {
+//           id: 'ethereum',
+//           current_price: 1683.43,
+//         },
+//         {
+//           id: 'polkadot',
+//           current_price: 4.49,
+//         }
+//       ])
+//     }, 0);
+//   })
+  
+// }
 
 
-function getTime() {
-  const currDate = new Date();
-  const currTime = currDate.getHours() + ':' + currDate.getMinutes() + ':' + currDate.getSeconds();
-  return currTime;
-}
+// async function storage () {
+//   console.log("fetching data");
+//   // const fetchedData = await myFetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+//   const fetchedData = await myFetch();
+//   console.log(fetchedData)
+//   console.log("data fetch");
+//   // const jsonData = await fetchedData.json();
+//   const coinArray = fetchedData.slice(0, 3);
+//   setCoins(coinArray);
+// }
+
+
 
 function App() {
-  // const [coins, setCoins] = useState([]);
-  // const [time, setTime] = useState(getTime());
-  // const [currentTime, setCurrentTime] = useState(getTime());
-  // const [fetchFlag, setFetchFlag] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   const timeUpdate = async function() {
-  //     setInterval(() => {
-  //       setCurrentTime(getTime());
-  //     }, 1000);
-  //   }
-  //   timeUpdate();
-  // })
-
-  // useEffect(() => {
-  //   const getData = async function() {
-  //     try{
-  //       const dataFetch = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
-  //       const dataJson = await dataFetch.json();
-  //       const coinsArray = dataJson.slice(0, 5);
-  //       setCoins(coinsArray);
-  //       setTime(getTime());
-  //       setError(null); 
-       
-  //     }catch (er){
-        
-  //     }  
-  //   }
-  //   getData();
-  // }, [fetchFlag])
-
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setFetchFlag(fetchFlag => !fetchFlag);
-  //   }, 120000)
-  //   return () => clearInterval(interval);
-  // }, [])
-
-
-
-  // const [coins, setCoins] = useState([]);
-  // const [time, setTime] = useState(getTime());
-  // const [currentTime, setCurrentTime] = useState(getTime());
-  // const [fetchFlag, setFetchFlag] = useState(true);
-  // const [error, setError] = useState(null);
-  // const [intervalTime, setIntervalTime] = useState(2*60*1000);
-
-
-  // useEffect(() => {
-  //   const getData = async function () {
-  //     try{
-  //       const dataFetch = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
-  //       const dataJson = await dataFetch.json();
-  //       const coinsArray = dataJson.slice(0, 5);
-  //       setCoins(coinsArray);
-  //       setTime(getTime());
-  //       setError(null); 
-  //       if(intervalTime === (10*60*1000)) {
-  //         setIntervalTime((2*60*1000));
-  //       }
-       
-  //     }catch (er){
-  //       console.log("Fetch Error has occured");
-  //       setError("Fetch Error has occured");
-  //       setIntervalTime(10*60*1000);
-  //     } 
-  //   }
-  //   getData();
-  // }, [fetchFlag, intervalTime])
-
-  // useEffect(() => {
-  //     const timeUpdate = async function() {
-  //       setInterval(() => {
-  //         setCurrentTime(getTime());
-  //       }, 1000);
-  //     }
-  //     timeUpdate();
-  //   })
-
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setFetchFlag((value) => !value)
-  //   },intervalTime)
-
-  //   return () => clearInterval(interval);
-  // },[intervalTime])
-
   const [coins, setCoins] = useState([]);
-  const [time, setTime] = useState(getTime());
-  const [currentTime, setCurrentTime] = useState(getTime());
   const [fetchFlag, setFetchFlag] = useState(true);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const timeUpdate = function() {
-      setInterval(() => {
-        setCurrentTime(getTime());
-      }, 1000);
-    }
-    timeUpdate();
-  }, [])
 
 
   useEffect(() => {
-    const lastCacheTime = localStorage.getItem(CACHE_TIME);
-    const presentTime = new Date().getTime();
-    if(lastCacheTime && (presentTime-lastCacheTime <= (4*60*1000))) {
-      console.log("Using cached data");
-      const cachedData = localStorage.getItem(CACHE_KEY);
-      if (cachedData) {
-        setCoins(JSON.parse(cachedData));
-        setError(null);
+
+    const fetchData = async function () {
+
+      const currentTime = new Date().getTime();
+      const cacheTime = localStorage.getItem(cacheTimeKey);
+
+      if(cacheTime === null) {
+        console.log("cache is empty");
+        // const fetchedData = await myFetch();
+        const fetchedData = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+        const jsonData = await fetchedData.json();
+        const coinArray = jsonData.slice(0, 3);
+        localStorage.setItem(cacheTimeKey, new Date().getTime());
+        localStorage.setItem(cacheCoins, JSON.stringify(coinArray));
+        setCoins(coinArray);
+      }else if((currentTime-cacheTime)>60000) {
+        console.log("cache has become old");
+        const fetchedData = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+        const jsonData = await fetchedData.json();
+        const coinArray = jsonData.slice(0, 3);
+        localStorage.setItem(cacheTimeKey, new Date().getTime());
+        localStorage.setItem(cacheCoins, JSON.stringify(coinArray));
+        setCoins(coinArray);
+      }else {
+        console.log("keep using the displayed value");
       }
+          
     }
-  }, [])
+    
+    
+    fetchData();
 
-  useEffect(() => {
-    const getData = async function() {
-      try{
-        const dataFetch = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en');
-        const dataJson = await dataFetch.json();
-        const coinsArray = dataJson.slice(0, 5);
-        localStorage.setItem(CACHE_KEY, JSON.stringify(coinsArray));
-        localStorage.setItem(CACHE_TIME, new Date().getTime());
-        setCoins(coinsArray);
-        setTime(getTime());
-        setError(null); 
-       
-      }catch (er){
-        setError("Fetch error has occured");
-      }  
-    }
-    const lastCacheTime = localStorage.getItem(CACHE_TIME);
-    const presentTime = new Date().getTime();
-    if(lastCacheTime === null) {
-      console.log("No previous cache data");
-      getData();
-    }else if(lastCacheTime && (presentTime-lastCacheTime >= (4*60*1000))) {
-      console.log("Updating cache data")
-      getData();
-    }else {
-      console.log("not fetching");
-    }
-  }, [fetchFlag])
-
+  },[fetchFlag])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setFetchFlag(fetchFlag => !fetchFlag);
-    }, 120000)
+      console.log("setting flag");
+      setFetchFlag(value => !value);
+      console.log("flag set")
+    }, (30000))
+
     return () => clearInterval(interval);
   }, [])
 
-
   return (
-    <>
-    <p>Current Time is : {currentTime}</p>
-    {error && <p className="error-message">{error}</p>}
-    {!error && (<div className="display">
-      <h2>Updates every 2 mins</h2>
+    <div>
       <ul>
         {coins.map((coin) => {
           return (
-            <li key={coin.id}>
-              <p>{coin.name} value (in USD) : {coin.current_price}</p>
-              <img src={coin.image} alt={coin.id} />
-            </li>
+            <li key={coin.id}>Value (USD) : {coin.current_price}</li>
           )
         })}
       </ul>
-      
-    </div>)}
-    <h3>Last updated: {time}</h3>
-    </>
+    </div>
   )
+
 }
 
 export default App
