2b8e52f85801bcedc1a6070c0fe8269ea745dab9
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-08-18 22:31:25 +0530

    Implements custom useCoinData hook.
    The states and useEffects being used to fetch coin's data
    is now extracted to a custom hook "useCoinData"
    useCoinData takes no argument and returns the latest state
    of coin data.

---
+++ src/components/useCoinData.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react'
+// import myFetch from './myFetch';
+
+const cacheTimeKey = 'cache_time';
+const cacheCoins = 'cache_coins';
+
+
+export default function useCoinData() {
+    const [coins, setCoins] = useState([]);
+    const [fetchFlag, setFetchFlag] = useState(true);
+  
+    useEffect(() => {
+      const cacheTime = localStorage.getItem(cacheTimeKey);
+      const currentTime = new Date().getTime();
+      if((currentTime-cacheTime) < 40000) {
+        const coinArray = JSON.parse(localStorage.getItem(cacheCoins));
+        setCoins(coinArray);
+      }   
+    }, [])
+  
+    useEffect(() => {
+  
+      const fetchData = async function () {
+  
+        const currentTime = new Date().getTime();
+        const cacheTime = localStorage.getItem(cacheTimeKey);
+  
+        if(cacheTime === null || (currentTime-cacheTime)>40000) {
+          console.log("Update cache");
+        //   const jsonData = await myFetch();
+          const fetchedData = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en");
+          const jsonData = await fetchedData.json();
+          const coinArray = jsonData.slice(0, 3);
+          localStorage.setItem(cacheTimeKey, new Date().getTime());
+          localStorage.setItem(cacheCoins, JSON.stringify(coinArray));
+          setCoins(coinArray);
+        }else {
+          console.log("keep using the old cache");
+        }
+            
+      }
+      
+      fetchData();
+  
+    },[fetchFlag])
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        console.log("setting flag");
+        setFetchFlag(value => !value);
+        console.log("flag set")
+      }, (20000))
+  
+      return () => clearInterval(interval);
+    }, [])
+
+    return coins;
+}
